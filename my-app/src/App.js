4c89f980792000916304fc1c1a33aa251e7b3625
schiZis <stahanovgod@gmail.com>
2021-12-29 16:29:22 +0500

    unfinished Events homework

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,51 @@
-import './App.css';
-import { Component } from 'react';
-import ConditionalClass from "./components/conditional_class/Conditional_Class";
-import ConditionalFunc from "./components/conditional_func/Conditional_Func";
+import React, { useState } from "react";
+import {hover} from "@testing-library/user-event/dist/hover";
 
-class App extends Component {
-    render() {
-        return (
-            <>
-                <ConditionalFunc/>
-                <ConditionalClass/>
-            </>
-        );
+const App = () => {
+    const [bg, setBg, setFilter, setWidth, setHeight] = useState("#fafafa");
+
+    const appStyles = {
+        height: "1440px",
+        background: `${bg}`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
     };
-};
 
-export default App
+    const initialStyle = {
+        width: "300px",
+        height: "400px",
+        fontSize: "25px",
+        borderRadius: "12px",
+        border: "1px transparent",
+        color: "#000000",
+        filter: "blur(0)",
+        backgroundImage: `url(https://i.redd.it/qkpntrbcsrd41.jpg)`,
+    };
+
+    const handleHover = (backgroundImage, filter) => {
+        setBg(backgroundImage);
+        setFilter(filter);
+    };
+
+    const handleClick = (width, height) => {
+        setWidth(width);
+        setHeight(height);
+    };
+
+    return (
+        <div style={appStyles}>
+            <button
+                style={initialStyle}
+                onMouseEnter={() => handleHover(`url(https://i.pinimg.com/originals/cf/da/3c/cfda3c9030bc6706e4c3509582e19526.jpg)`, "blur(4px)")}
+                onMouseLeave={() => handleHover("#fafafa")}
+                onClick={() => handleClick("600px", "800px")}
+            >
+                {" "}
+                Hover and Click
+            </button>
+        </div>
+    );
+}
+
+export default App;
