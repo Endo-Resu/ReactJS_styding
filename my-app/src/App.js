b3dbccab1dc160a16dbd97294eea365eb64791f4
schiZis <stahanovgod@gmail.com>
2022-01-04 23:40:14 +0500

    editing tasks + filter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,21 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo"
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        tasks: this.props.tasks
+        tasks: this.props.tasks,
+            filter: 'All'
         };
-    }
-
-    componentDidMount() {
-        this.setState({
-            tasks: this.props.tasks
-        });
     }
 
     addTask(name) {
@@ -45,6 +48,19 @@ class App extends Component {
         });
     }
 
+    editTask(id, newName) {
+        const editedTaskList = this.state.tasks.map(task => {
+            if (id === task.id) {
+                return {...task, name: newName}
+            }
+            return task;
+        });
+        this.state.tasks.push(editedTaskList)
+        this.setState({
+            tasks: editedTaskList
+        });
+    }
+
     render() {
         this.tasksNoun = this.state.tasks.length !== 1 ? 'tasks' : 'task';
         this.headingText = `${this.state.tasks.length} ${this.tasksNoun} remaining`;
@@ -53,9 +69,14 @@ class App extends Component {
                 <h1>ToDo App</h1>
                 <Form addTask={this.addTask.bind(this)}/>
                 <div className="filters btn-group stack-exception">
-                    <FilterButton />
-                    <FilterButton />
-                    <FilterButton />
+                    {FILTER_NAMES.map(name => (
+                        <FilterButton
+                            key={name}
+                            name={name}
+                            isPressed={name === this.props.filter}
+                            setFilter={this.props.setFilter}
+                        />
+                    ))}
                 </div>
                 <h2 id="list-heading">
                     {this.headingText}
@@ -72,6 +93,7 @@ class App extends Component {
                             key={task.id}
                             toggleTaskCompleted={this.toggleTaskCompleted.bind(this)}
                             deleteTask={this.deleteTask.bind(this)}
+                            editTask={this.editTask.bind(this)}
                         />
                     ))}
                 </ul>
--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -4,36 +4,75 @@ class Todo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: this.props.id
+            id: this.props.id,
+            isEditing: this.props.isEditing
         }
     }
+
     render() {
+        const editingTemplate = (
+            <div>
+                <form className="stack-small">
+                    <div className="form-group">
+                        <label className="todo-label" htmlFor={this.props.id}>
+                            New name for {this.props.name}
+                        </label>
+                        <input id={this.props.id} className="todo-text" type="text" />
+                    </div>
+                    <div className="btn-group">
+                        <button
+                            type="button"
+                            className="btn todo-cancel"
+                            onClick={() => this.props.setEditing(this.props.isEditing)}
+                        >
+                            Cancel
+                            <span className="visually-hidden">renaming {this.props.name}</span>
+                        </button>
+                        <button type="submit" className="btn btn__primary todo-edit">
+                            Save
+                            <span className="visually-hidden">new name for {this.props.name}</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+
+        const viewTemplate = (
+            <div>
+                <div className="stack-small">
+                    <div className="c-cb">
+                        <input
+                            id={this.props.id}
+                            type="checkbox"
+                            defaultChecked={this.props.completed}
+                            onChange={() => this.props.toggleTaskCompleted(this.props.id)}
+                        />
+                        <label className="todo-label" htmlFor={this.props.id}>
+                            {this.props.name}
+                        </label>
+                    </div>
+                    <div className="btn-group">
+                        <button
+                            type="button"
+                            className="btn"
+                            onClick={() => this.props.editTask(this.props.id)}
+                        >
+                            Edit <span className="visually-hidden">{this.props.name}</span>
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn__danger"
+                            onClick={() => this.props.deleteTask(this.props.id)}
+                        >
+                            Delete <span className="visually-hidden">{this.props.name}</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+
         return (
-            <li className="todo stack-small">
-                <div className="c-cb">
-                    <input
-                        id={this.props.id}
-                        type="checkbox"
-                        defaultChecked={this.props.completed}
-                        onChange={() => this.props.toggleTaskCompleted(this.props.id)}
-                    />
-                    <label className="todo-label" htmlFor={this.props.id}>
-                        {this.props.name}
-                    </label>
-                </div>
-                <div className="btn-group">
-                    <button type="button" className="btn">
-                        Edit <span className="visually-hidden">{this.props.name}</span>
-                    </button>
-                    <button
-                        type="button"
-                        className="btn btn__danger"
-                        onClick={() => this.props.deleteTask(this.props.id)}
-                    >
-                        Delete <span className="visually-hidden">{this.props.name}</span>
-                    </button>
-                </div>
-            </li>
+            <li className="todo">{this.isEditing ? editingTemplate : viewTemplate}</li>
         );
     }
 }
