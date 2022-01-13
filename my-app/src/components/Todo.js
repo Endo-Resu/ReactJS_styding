fe82689edce9c9ee844a55681a20cfea7a2ea57e
schiZis <stahanovgod@gmail.com>
2022-01-14 00:25:53 +0500

    splited templates

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import EditingTemplate from './templates/EditingTemplate'
+import ViewTemplate from './templates/ViewTemplate'
 
 class Todo extends Component {
     constructor(props) {
@@ -9,78 +11,28 @@ class Todo extends Component {
             newName: "",
         }
     }
-    newNameHandler(e) {
-        this.setState({
-            newName: e.target.value
-        })
-    }
+
     render() {
-        const editingTemplate = (
-            <div>
-                <form className="stack-small" onSubmit={(e) => e.preventDefault()}>
-                    <div className="form-group">
-                        <label className="todo-label" htmlFor={this.props.id}>
-                            New name for {this.props.name}
-                        </label>
-                        <input
-                            onInput={(event) => this.newNameHandler.call(this, event)}
-                            value={this.state.newName}
-                            id={this.props.id}
-                            className="todo-text"
-                            type="text" />
-                    </div>
-                    <div className="btn-group">
-                        <button
-                            type="button"
-                            className="btn todo-cancel"
-                            onClick={() => this.props.setEditing(this.props.id, false)}
-                        >
-                            Cancel
-                            <span className="visually-hidden">renaming {this.props.name}</span>
-                        </button>
-                        <button  onClick={() => this.props.editTask(this.props.id, this.state.newName)}  className="btn btn__primary todo-edit">
-                            Save
-                            <span className="visually-hidden">new name for {this.props.name}</span>
-                        </button>
-                    </div>
-                </form>
-            </div>
-        );
-        const viewTemplate = (
-            <div>
-                <div className="stack-small">
-                    <div className="c-cb">
-                        <input
-                            id={this.props.id}
-                            type="checkbox"
-                            defaultChecked={this.props.completed}
-                            onChange={() => this.props.toggleTaskCompleted(this.props.id)}
-                        />
-                        <label className="todo-label" htmlFor={this.props.id}>
-                            {this.props.name}
-                        </label>
-                    </div>
-                    <div className="btn-group">
-                        <button
-                            type="button"
-                            className="btn"
-                            onClick={() => this.props.setEditing(this.props.id, true)}
-                        >
-                            Edit <span className="visually-hidden">{this.props.name}</span>
-                        </button>
-                        <button
-                            type="button"
-                            className="btn btn__danger"
-                            onClick={() => this.props.deleteTask(this.props.id)}
-                        >
-                            Delete <span className="visually-hidden">{this.props.name}</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
         return (
-            <li className="todo">{this.props.isEditing ? editingTemplate : viewTemplate}</li>
+            <li className="todo"> {
+                this.props.isEditing
+                    ?<EditingTemplate
+                        id={this.props.id}
+                        editTask={this.props.editTask}
+                        complited={this.props.completed}
+                        name={this.props.name}
+                        setEditing={this.props.setEditing}
+                        toggleTaskCompleted={this.props.toggleTaskCompleted}
+                    />
+                    : <ViewTemplate
+                        toggleTaskCompleted={this.props.toggleTaskCompleted}
+                        name={this.props.name}
+                        setEditing={this.props.setEditing}
+                        deleteTask={this.props.deleteTask}
+                        id={this.props.id}
+                        complited={this.props.completed}
+                    />}
+            </li>
         );
     }
 }
